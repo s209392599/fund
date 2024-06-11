75d209978bf73aab64c48de28708552641aab917
郭坤 <209392599@qq.com>
2024-06-11 21:39:22 +0800

    车

--- data/data.js
+++ data/data.js
@@ -2,17 +2,17 @@ module.exports = {
   // 已买
   yimai: [
     { "number": "007677", "name": "蜂巢添汇(7天)", "remarks": "10.60", "notice": "", skuId: '1007677', status: '1' },
+    { "number": "009461", "name": "东方臻萃(3)(5-6)", "remarks": "8.5;入8", "notice": "", skuId: '111156', status: '3' },
+    { "number": "400030", "name": "东方添益(随时)", "remarks": "6.62;入8;730", "notice": "", skuId: '106545', status: '2' },
   ],
   // 待移除
   daiyichu: [
-    { "number": "400030", "name": "东方添益(随时)", "remarks": "6.62;入8;730", "notice": "", skuId: '106545', status: '2' },
     { "number": "485119", "name": "工银信用(稳)", "remarks": "5.96;入8;730", "notice": "", skuId: '107337', status: '2' },
     { "number": "006980", "name": "国寿安保(限500)", "remarks": "6.98;入8;30", "notice": "", skuId: '114237', status: '2' },
     { "number": "003547", "name": "鹏华丰禄(限100)", "remarks": "5.46;入8;365", "notice": "", skuId: '110067', status: '2' },
   ],
   // 待买
   daimai: [
-    { "number": "009461", "name": "东方臻萃(3)(5-6)", "remarks": "8.5;入8", "notice": "", skuId: '111156', status: '3' },
     { "number": "007091", "name": "东兴兴福(1)(7-1)", "remarks": "9.74;入6", "notice": "", skuId: '109726', status: '3' },
     { "number": "007769", "name": "东兴兴瑞(1)(11-11)", "remarks": "9.68;入6", "notice": "", skuId: '112288', status: '3' },
     { "number": "006760", "name": "国金惠盈纯债C(30)", "remarks": "7.77;006549是A类", "notice": "", "skuId": "109475", status: '3' },
@@ -22,15 +22,12 @@ module.exports = {
     { "number": "009604", "name": "国金惠盈(1000)", "remarks": "7天", "notice": "", skuId: '1009604', status: '4' },
     { "number": "007214", "name": "国泰惠丰(30天)", "remarks": "", "notice": "", skuId: '1007214', status: '4' },
     { "number": "000116", "name": "嘉实丰益(1)(10.21)", "remarks": "", "notice": "", skuId: '107795', status: '4' },
-    { "number": "005070", "name": "长江乐丰(3)(6.11)", "remarks": "", "notice": "", skuId: '1005070', status: '4' },
-    { "number": "006716", "name": "东方永泰(1)(5.23)", "remarks": "", "notice": "", skuId: '109338', status: '4' },
     { "number": "519762", "name": "交银裕通", "remarks": "", "notice": "", skuId: '108617', status: '4' },
 
     { "number": "007540", "name": "华泰保兴安悦A", "remarks": "8.95", "notice": "", "skuId": "1007540", status: '5' },
     { "number": "017593", "name": "汇添富添添乐C", "remarks": "8.65", "notice": "", "skuId": "1017593", status: '5' },
     { "number": "000306", "name": "天弘弘利债券A", "remarks": "7.96", "notice": "", "skuId": "112431", status: '5' },
     { "number": "008798", "name": "国金惠安利A", "remarks": "7.79", "notice": "", "skuId": "110736", status: '5' },
-    { "number": "008799", "name": "国金惠安利C", "remarks": "7.65", "notice": "", "skuId": "110735", status: '5' },
     { "number": "017045", "name": "达诚腾益A", "remarks": "7.23", "notice": "", "skuId": "1017045", status: '5' },
     { "number": "010353", "name": "南方崇元A", "remarks": "7.19", "notice": "", "skuId": "113391", status: '5' },
     { "number": "011249", "name": "嘉实稳裕A", "remarks": "7.17", "notice": "", "skuId": "1011249", status: '5' }
@@ -39,6 +36,8 @@ module.exports = {
   fangqi: [
     { "number": "485019", "name": "工银信用(稳)", "remarks": "已经有同类的了 2024年04月21日16:24:15", "notice": "", skuId: '107328', status: '6' },
     { "number": "005637", "name": "国联聚业(3月)", "remarks": "只能机构购买", "notice": "", skuId: '1005637', status: '6' },
+    { "number": "005070", "name": "长江乐丰(3)", "remarks": "", "notice": "只能机构购买", skuId: '1005070', status: '4' },
+    { "number": "006716", "name": "东方永泰(1)(5.23)", "remarks": "", "notice": "车日楠的有了", skuId: '109338', status: '4' },
     { "number": "007925", "name": "平安鑫享混合E", "remarks": "8.68", "notice": "", "skuId": "111377", status: '6' },
     { "number": "002988", "name": "平安鼎信(30天)", "remarks": "", "notice": "", skuId: '111847', status: '6' },
     { "number": "007235", "name": "广发聚利(30天)", "remarks": "", "notice": "", skuId: '1007235', status: '6' },
