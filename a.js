00844403da01dda9f61b5cb7b41400d50443f156
郭坤 <209392599@qq.com>
2024-09-17 18:08:44 +0800

    new

---
+++ a.js
@@ -0,0 +1,24 @@
+/* 
+name: Deploy to GitHub Pages
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v2
+
+      - name: Deploy to GitHub Pages
+        uses: peaceiris/actions-gh-pages@v3
+        with:
+          github_token: ${{ secrets.GITPAGETOKEN }}
+          publish_dir: ./dist
+          publish_branch: gh-pages
+
+          
+*/
