7e8208f16a77f4f5fe7eed9371ec26f0f95cdaac
郭坤 <209392599@qq.com>
2024-12-10 23:20:49 +0800

    近来三月

--- data/today.js
+++ data/today.js
@@ -8,7 +8,7 @@ var arr = [...yimai];
 async function getFund(code, index) {
   console.log(`正在请求第 ${index + 1} 个基金数据 ~~~`);
   // https://lc.jr.jd.com/finance/fund/latestdetail/achievement/?fundCode=400030&disclosureType=1&activeIndex=2
-  let u = `https://ms.jr.jd.com/gw/generic/jj/h5/m/getFundHistoryNetValuePageInfo?reqData={"fundCode":"${code}","pageNum":1,"pageSize":23,"channel":"9"}`;
+  let u = `https://ms.jr.jd.com/gw/generic/jj/h5/m/getFundHistoryNetValuePageInfo?reqData={"fundCode":"${code}","pageNum":1,"pageSize":69,"channel":"9"}`;
   return fetch(u, {})
     .then((res) => res.json())
     .then((res) => {
@@ -36,16 +36,19 @@ async function fetchFundData() {
     let fund_data_arr = await getFund(arr[i].number, i);
 
     let obj_1 = fund_data_arr[0];
-    let obj_2 = fund_data_arr[fund_data_arr.length - 1];
+    let obj_2 = fund_data_arr[22];// 近一月
+    let obj_3 = fund_data_arr[fund_data_arr.length - 1];
     // 计算差值
-    let total_cha = Math.floor(obj_1.totalNetValue * 10000) - Math.floor(obj_2.totalNetValue * 10000);
+    let total_1_month = Math.floor(obj_1.totalNetValue * 10000) - Math.floor(obj_2.totalNetValue * 10000);
+    let total_3_month = Math.floor(obj_1.totalNetValue * 10000) - Math.floor(obj_3.totalNetValue * 10000);
 
     const flag = obj_1.date === cur_date;
     IncreaseArr.push({
       代号: arr[i].number,
       名称: arr[i].name,
       万元收入: flag ? Math.round(Number(obj_1.dailyProfit) * 100) : 0,
-      近一月: total_cha,
+      近一月: total_1_month,
+      近三月: total_3_month,
       // 单位净值: flag ? obj_1.netValue : '',
       // 累计净值: flag ? obj_1.totalNetValue : '',
       日期: flag ? obj_1.date : '',
