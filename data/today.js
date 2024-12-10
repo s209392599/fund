19890011e33d6b732cc7733134e6b5e46ccd89f4
郭坤 <209392599@qq.com>
2024-12-10 23:11:56 +0800

    添加近一个月累计

--- data/today.js
+++ data/today.js
@@ -8,21 +8,20 @@ var arr = [...yimai];
 async function getFund(code, index) {
   console.log(`正在请求第 ${index + 1} 个基金数据 ~~~`);
   // https://lc.jr.jd.com/finance/fund/latestdetail/achievement/?fundCode=400030&disclosureType=1&activeIndex=2
-  let u = `https://ms.jr.jd.com/gw/generic/jj/h5/m/getFundHistoryNetValuePageInfo?reqData={"fundCode":"${code}","pageNum":1,"pageSize":20,"channel":"9"}`;
+  let u = `https://ms.jr.jd.com/gw/generic/jj/h5/m/getFundHistoryNetValuePageInfo?reqData={"fundCode":"${code}","pageNum":1,"pageSize":23,"channel":"9"}`;
   return fetch(u, {})
     .then((res) => res.json())
     .then((res) => {
       let resultData = res.resultData || {};
       let datas = resultData.datas || {};
       let netValueList = datas.netValueList || [];
+      // console.log(`today.js 18 [netValueList]`,netValueList);
       // '{"date":"2024-03-26","netValue":"1.3149","dailyProfit":"-0.02","totalNetValue":"1.5429"}'
-      // '{"date":"2024-03-26","netValue":"1.3149","dailyProfit":"-0.02","totalNetValue":"1.5429"}'
-      return netValueList[0] || {};
+      return netValueList || [];
     });
 }
 
 var IncreaseArr = [];
-
 // Number((Number("-0.07") * 100).toFixed(0))
 
 async function fetchFundData() {
@@ -34,15 +33,22 @@ async function fetchFundData() {
   const cur_date = `${cur_year}-${cur_month}-${cur_day}`;
 
   for (let i = 0; i < arr.length; i++) {
-    let obj = await getFund(arr[i].number, i);
-    const flag = obj.date === cur_date;
+    let fund_data_arr = await getFund(arr[i].number, i);
+
+    let obj_1 = fund_data_arr[0];
+    let obj_2 = fund_data_arr[fund_data_arr.length - 1];
+    // 计算差值
+    let total_cha = Math.floor(obj_1.totalNetValue * 10000) - Math.floor(obj_2.totalNetValue * 10000);
+
+    const flag = obj_1.date === cur_date;
     IncreaseArr.push({
       代号: arr[i].number,
       名称: arr[i].name,
-      万元收入: flag ? Math.round(Number(obj.dailyProfit) * 100) : 0,
-      // 单位净值: flag ? obj.netValue : '',
-      累计净值: flag ? obj.totalNetValue : '',
-      日期: flag ? obj.date : '',
+      万元收入: flag ? Math.round(Number(obj_1.dailyProfit) * 100) : 0,
+      近一月: total_cha,
+      // 单位净值: flag ? obj_1.netValue : '',
+      // 累计净值: flag ? obj_1.totalNetValue : '',
+      日期: flag ? obj_1.date : '',
     });
   }
 }
