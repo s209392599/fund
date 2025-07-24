dfbc794094ce9bde5cd93c9dfc05bb24ee5f770d
郭坤 <209392599@qq.com>
2025-07-24 16:28:14 +0800

    暂时记录

--- 记录.js
+++ 记录.js
@@ -9,7 +9,28 @@ https://m.dayfund.cn/ajs/ajaxdata.shtml?showtype=getfundvalue&fundcode=018561
 今日涨幅 | 今日净值变化
 -0.05%  | -0.0011|2.0467|2.0471|2025-07-23|11:00:00
 
+https://fundgz.1234567.com.cn/js/018561.js
+https://fundgz.1234567.com.cn/js/018561.js?rt=1753342851359
+https://fundgz.1234567.com.cn/js/018561.js?rt=1753257600000
+"jzrq":"2025-07-23","dwjz":"2.0433","gsz":"2.0552","gszzl":"0.58","gztime":"2025-07-24 15:00"});
 
 还有一个就是找找过年时各个基金的数据看下情况，看过年要不要清
+
+
+
+可以看到很多数据
+https://fund.eastmoney.com/pingzhongdata/019260.js?v=20250724153651
+
+实时涨幅
+https://fundgz.1234567.com.cn/js/019260.js?rt=1753342851359
+
+https://image.sinajs.cn/newchart/v5/fundpre/min_s/008164.gif 涨幅
+
+https://basic.10jqka.com.cn/JPZV30/ 可以按这个展示格式扣下数据
+
+
+买卖规则
+https://ms.jr.jd.com/gw/generic/jj/h5/m/getFundTradeRulesPageInfo?reqData={"fundCode":"515450","orderLimit":"","channel":"9"}
+
 
 */
