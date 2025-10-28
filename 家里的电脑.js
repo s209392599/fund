04c411343b6b4dbc1bf382cd93576b8d1b04d11b
郭坤 <209392599@qq.com>
2025-10-28 22:17:48 +0800

    Create 家里的电脑.js

---
+++ 家里的电脑.js
@@ -0,0 +1,119 @@
+/*
+https://www.bilibili.com/video/BV1JWtDeqE44/?vd_source=05a30f42f608d43de22f6db10d8c8d4d 软件讲解
+https://space.bilibili.com/381896465
+
+https://xuangu.eastmoney.com/Result?type=fund&color=w&id=xc0d67218dd193006da8&a=edit_way  条件选股
+https://xuangu.eastmoney.com/?type=fund&color=w
+
+https://cn.investing.com/funds/major-funds 英为财情
+https://cn.investing.com/charts/indices-charts
+
+
+https://lc.jr.jd.com/finance/funddetail/home/?fundCode=008327 京东金融详情 4个头部标签的种类，可以记录到爬取的js中去
+https://lc.jr.jd.com/finance/funddetail/home/?fundCode=007467
+https://lc.jr.jd.com/finance/funddetail/home/?fundCode=007540
+https://lc.jr.jd.com/finance/funddetail/home/?fundCode=002112
+
+自由现金流 搜索对比一下
+
+红利过期ETF  4.5%
+万家中证ETF 4.2%
+红利港股ETF 5.86%
+现金流ETF  4.23%,年化收益率20%+
+
+港股通红利低波ETF（520890）
+港股通央企红利
+
+https://space.bilibili.com/381896465  他的视频可以看下
+
+
+
+基金排行---收集一下两个种类的不同，一个可以node-fetch
+https://fund.eastmoney.com/data/fundranking.html#tall;c0;r;s1nzf;pn50;ddesc;qsd20240918;qed20250918;qdii;zq;gg;gzbd;gzfs;bbzt;sfbb
+https://fund.eastmoney.com/trade/hh.html#zwf_,sc_z,st_desc
+https://m.1234567.com.cn/index.html?page=jjph&tab=qb  手机端上的排行
+
+
+SELECT * FROM fund_user_collection WHERE fund_user_id=57 ORDER BY sort_order ASC;
+
+UPDATE fund_user_collection SET
+fund_code = '008087',
+fund_name = 'test',
+sort_order = 2,
+fundgz = 2,
+fund_type = 'type1',
+fund_sign = '正常',
+zhang_url = 'https',
+point_top = 1.5,
+point_down = 1.23,
+fixed = 100.56,
+fund_desc = '备注'
+WHERE id = 1;
+
+SELECT * FROM fund_public ORDER BY sort_order ASC;
+
+SELECT * FROM fund_users WHERE user_name = 'boxue' AND user_password = 'qaz123..';
+
+SELECT * FROM fund_users WHERE user_name = 'boxue' AND user_password = 'qaz123..';
+
+广发全球精选股票(QDII)人民币A(270023)
+
+亚太精选
+https://fund.eastmoney.com/data/fundsearch.html?spm=search&key=%E4%BA%9A%E5%A4%AA%E7%B2%BE%E9%80%89#key%E4%BA%9A%E5%A4%AA%E7%B2%BE%E9%80%89
+
+
+
+
+https://fundgz.1234567.com.cn/js/007467.js
+
+
+{"rankList":["连续5年跑赢同类 · 第9名"],"highlights":{"tagList":[],"morningstarRating":"2"},"userFocus":[],"themeNameList":["综合","免税店"]}
+[
+  {"avg":"0.13","rate":"-0.91","name":"近1周","rank":"3176/4354"},
+  {"avg":"7.53","rate":"4.32","name":"近1月","rank":"2737/4279"},
+  {"avg":"24.15","rate":"13.68","name":"近3月","rank":"3152/4019"},
+  {"avg":"18.01","rate":"8.82","name":"近6月","rank":"2775/3645"},
+  {"avg":"59.71","rate":"32.16","name":"近1年","rank":"2495/3020"},
+  {"avg":"24.13","rate":"9.87","name":"今年以来","rank":"3474/4354"},
+  {"avg":"26.69","rate":"21.93","name":"近3年","rank":"1061/1869"},
+  {"avg":"155.06","rate":"194.57","name":"成立以来","rank":"25/123"}
+]
+87|57|93|84
+93.54|0.00|7.86|0.00|0.00
+{
+  "stock":[
+    {"name":"洛阳钼业","industryName":"工业金属","ratio":"2.09%"},
+    {"name":"江苏银行","industryName":"银行","ratio":"2.07%"},
+    {"name":"民生银行","industryName":"银行","ratio":"1.97%"},
+    {"name":"潍柴动力","industryName":"汽车零部件","ratio":"1.91%"},
+    {"name":"光大银行","industryName":"银行","ratio":"1.90%"},
+    {"name":"中国联通","industryName":"通信服务","ratio":"1.89%"},
+    {"name":"华能国际","industryName":"电力","ratio":"1.89%"},
+    {"name":"平安银行","industryName":"银行","ratio":"1.88%"},
+    {"name":"三七互娱","industryName":"游戏","ratio":"1.87%"},
+    {"name":"三一重工","industryName":"工程机械","ratio":"1.87%"}
+  ],
+  "bond":[],
+  "fund":[]
+}
+{
+  "establishedDateByCn":"2013年03月22日",
+  "company_name":"财通基金管理有限公司",
+  "fundScaleList":["0.71","0.68","0.65","0.39"],
+  "instPurchaseRatio":"0",
+  "purchaseRatio":"100",
+  "companyManageScale":"753.33亿",
+  "manageNumber":"57"
+}
+[
+  {"yearPerformance":"-3.22",
+  "employPerformance":"17.32",
+  "employmentDate":"4年117天",
+  "accessionDateDesc":"2023.01.16-至今",
+  "managerName":"顾弘原",
+  "accessionDate":"2年247天",
+  "manageScale":"8.97亿元"}
+]
+39211615.71
+
+*/
