d9d2164c44be81d49de7da3dd20525b808a36683
郭坤 <209392599@qq.com>
2026-02-03 13:46:36 +0800

    备注

--- 系统功能优化.js
+++ 系统功能优化.js
@@ -10,13 +10,20 @@ http://150.158.175.108:9999/preview.html 图片要加上时间戳的后缀
 203812677的账户里面，显示一些历史的监控
 放在历史分组里面？
 
-
-中保信诚
-雅文妈妈的建信
-008164这个基金
-
-
 查看、上传 或者删除文件 可以指定路径
+
+
+这个也main是不是每天都要看下 2026年02月03日13:33:04
+https://www.cls.cn/quotation
+https://www.cls.cn/finance
+注：手机上 ETF*基金 这个tab下的板块的链接是多少？ 用穿透工具获取一下，电脑上找不到
+
+CPO量化的基金
+https://api3.cls.cn/quote/fundplate?os=ios&fundTypeCode=cls81935
+
+基金排行榜
+https://api3.cls.cn/quote/fund/0/0?os=ios
+https://api3.cls.cn/quote/fund/1/0?os=ios&sv=817&token= 白酒排行榜
 
 
 
